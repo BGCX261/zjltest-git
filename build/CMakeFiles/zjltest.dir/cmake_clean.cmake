7209a347c1699a8ef26460d0d864bccbf24dcfce
ZJ Laczik <Laczik@eng.ox.ac.uk>
2012-03-21 21:45:18 +0000

    First import

---
+++ build/CMakeFiles/zjltest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/zjltest.dir/main.cpp.o"
+  "zjltest.pdb"
+  "zjltest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/zjltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
